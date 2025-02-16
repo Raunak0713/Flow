21aaa965f3bbbfad4645be1421a64f911ef7c9cd
Raunak0713 <raunakbhanarkar7@gmail.com>
2025-02-16 11:43:12 +0530

    #3 Sidebar Enhancement & Logo Integration

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+import BreadcrumbHeader from "@/components/BreadcrumbHeader"
 import DesktopSidebar from "@/components/Sidebar"
 import { Separator } from "@/components/ui/separator"
 
@@ -11,7 +12,7 @@ const DashboardLayout = ({ children } : DashboardLayoutProps) => {
       <DesktopSidebar />
       <div className="flex flex-col flex-1 min-h-screen">
         <header className="flex items-center justify-between px-6 py-4 h-[50px] container">
-          ScrapeFlow
+          <BreadcrumbHeader />
         </header>
         <Separator />
         <div className="overflow-auto">
---
+++ components/BreadcrumbHeader.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BreadcrumbHeader = () => {
+  return (
+    <div>BreadcrumbHeader</div>
+  )
+}
+
+export default BreadcrumbHeader
